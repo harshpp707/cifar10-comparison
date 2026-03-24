f60bcc25e48a812e8cb717060085652530cfea6f
MitchellParks29 <bmitchellparks@gmail.com>
2026-03-23 21:30:30 -0500

    Updated CNN Model

--- models/cnn.py
+++ models/cnn.py
@@ -8,27 +8,33 @@ class SimpleCNN(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
         self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(2,2)
 
         self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
         self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
-        self.pool2 = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(64 * 8 * 8, 256)
-        self.fc2 = nn.Linear(256, num_classes)
+        self.conv5 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=1)
 
+        self.fc1 = nn.Linear(128*4*4, 512)
         self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(512, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = self.pool1(x)
+        x = self.pool(x)
 
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
-        x = self.pool2(x)
+        x = self.pool(x)
+
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.pool(x)
 
         x = torch.flatten(x, 1)
+
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
